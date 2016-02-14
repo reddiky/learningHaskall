a841cad4a6a92cbf2c79df113e68700ba129cd2e
Reddiky <kyle@reddiky.com>
2016-02-13 21:55:56 -0500

    beginning to learn Haskall

---
+++ baby.hs
@@ -0,0 +1,12 @@
+doubleMe x = x + x
+doubleUs x y  = x * 2 + y * 2
+doubleSmallNumber  x =  if x > 100 then x else x*2
+
+boomBangs xs = [if x < 10 then "Boom!" else "BANG!" | x<-xs, odd x, x/=3, x/=5, x/=7]
+
+
+filterLowers st = [c | c <- st, c `elem`  ['A'..'Z']]
+
+sumLowers st = sum [1 | _ <- [c | c <- st, elem c ['a'..'z']]]
+
+rightTriangles = [(a,b,c) |  c <- [1..10], a <- [1..c], b <- [1..a], a ^2 + b ^ 2 == c^2, a+b+c==24]
